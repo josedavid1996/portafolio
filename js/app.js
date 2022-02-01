08caa143f4a7a9eba6a21c5d15e3bcd3a83ff3bd
Jose david Redondo <redondoperdomojosedavid@gmail.com>
2022-02-01 18:46:48 -0500

    Agregue los dos proyectos de react y le puse el targe a los enlaces

--- js/app.js
+++ js/app.js
@@ -1,11 +1,40 @@
 ((d) => {
   const $btnButton = d.querySelector(".menu__btn"),
     $menu = d.querySelector(".menu");
-
   $btnButton.addEventListener("click", (e) => {
     $btnButton.firstElementChild.classList.toggle("none");
     $btnButton.lastElementChild.classList.toggle("none");
-
     $menu.classList.toggle("is-active");
   });
+  function mostrar() {
+    const $boton = document.getElementById("boton");
+    document.addEventListener("click", (e) => {
+      if (e.target === $boton) {
+        const alert = Swal.fire({
+          icon: "success",
+          title: "Gracias por tu interes te contactare lo antes posible",
+          text: "Enviado!",
+        });
+      }
+    });
+  }
+  const $form = document.getElementById("form");
+  $form.addEventListener("submit", hanlessubmit);
+  async function hanlessubmit(event) {
+    event.preventDefault();
+    const form = new FormData(this);
+    const response = await fetch(this.action, {
+      method: this.method,
+      body: form,
+      headers: { Accept: "Aplication/json" },
+    });
+    if (response.ok) {
+      this.reset();
+      Swal.fire({
+        icon: "success",
+        title: "Gracias por tu interes te contactare lo antes posible",
+        text: "Enviado!",
+      });
+    }
+  }
 })(document);
